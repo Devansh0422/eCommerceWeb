7da90294a8d61e1896f0b26f2216a0b68c1c7c97
Devansh Jaiswal <djdevansh01@gmail.com>
2026-01-16 00:53:36 +0530

    first push

--- src/main/java/com/eCommerceWebsite/eCommerceWeb/service/CategoryServiceImpl.java
+++ src/main/java/com/eCommerceWebsite/eCommerceWeb/service/CategoryServiceImpl.java
@@ -27,7 +27,6 @@ public class CategoryServiceImpl implements CategoryService {
     public String deleteCategory(Long categoryId) {
         Category category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst().orElse(null);
         if (category == null) {return "not found";}
-
         categories.remove(category);
         return "Category with id "+categoryId+" is deleted";
     }
