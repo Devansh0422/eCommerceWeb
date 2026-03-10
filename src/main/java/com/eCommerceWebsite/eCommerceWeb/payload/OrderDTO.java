828dbd8672c534342f5fbcb4e15ced2d55c92202
Devansh Jaiswal <djdevansh01@gmail.com>
2026-03-11 00:33:00 +0530

    payment page

--- src/main/java/com/eCommerceWebsite/eCommerceWeb/payload/OrderDTO.java
+++ src/main/java/com/eCommerceWebsite/eCommerceWeb/payload/OrderDTO.java
@@ -1,4 +1,24 @@
 package com.eCommerceWebsite.eCommerceWeb.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDTO {
+
+    private Long orderId;
+    private String email;
+    private LocalDate orderDate;
+    private PaymentDTO payment;
+    private List<OrderItemDTO> orderItems;
+    private Double totalAmount;
+    private String orderStatus;
+    private Long addressId;
+
 }
--- src/main/java/com/eCommerceWebsite/eCommerceWeb/payload/OrderRequestDTO.java
+++ src/main/java/com/eCommerceWebsite/eCommerceWeb/payload/OrderRequestDTO.java
@@ -1,4 +1,19 @@
 package com.eCommerceWebsite.eCommerceWeb.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderRequestDTO {
+
+    private Long addressId;
+    private String pgName;
+    private String pgResponseMessage;
+    private String pgStatus;
+    private String  paymentMethod;
+    private  String pgPaymentId;
+
 }
